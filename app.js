b55ff0bae9e353fc37a6a3784f58bcffc4bc8ef3
Alvaro Martinez <alvaromartinez@MacBook-Pro-de-Alvaro.local>
2018-02-12 19:03:20 +0100

    más cambios

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
 app.use(session({
   secret: "our-passport-local-strategy-app",
   resave: true,
--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,8 @@ const express = require('express');
 const Event = require('../models/Event');
 const TYPES = require('../models/Event-types');
 const router = express.Router();
-const { ensureLoggedIn } = require('connect-ensure-login');
+const isLoggedIn = require('../middlewares/isLoggedIn');
+const onlyMe = require('../middlewares/onlyMe');
 
 router.get('/', (req, res, next) => {
 
@@ -18,8 +19,9 @@ router.get('/new', (req, res) => {
     res.render('events/new', { types: TYPES });
   });
   
-  //router.post('/new', ensureLoggedIn('/login'), (req, res, next) => {
-  router.post('/new', (req, res, next) => {
+  //router.post('/new', isLoggedIn('/login'), (req, res, next) => {
+    router.post('/new', (req, res, next) => {
+      console.log(req.sesionID)
     const newEvent = new Event({
       title: req.body.title,
       goal: req.body.goal,
@@ -39,13 +41,13 @@ router.get('/new', (req, res) => {
       });
   });
   
-  router.get('/:id', (req, res, next) => {
+  router.get(':id', (req, res, next) => {
     Event.findById(req.params.id, (err, event) => {
       if (err){ return next(err); }
   
       event.populate('_creator', (err, event) => {
         if (err){ return next(err); }
-        return res.render('home/show', { event });
+        return res.render('events/show', { event });
       });
     });
   });
