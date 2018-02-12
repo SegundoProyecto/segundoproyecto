2c10f920fd3dc267eaf1294cfa55e9b422bf0ea8
Alvaro Martinez <alvaromartinez@MacBook-Pro-de-Alvaro.local>
2018-02-12 15:05:46 +0100

    Local strategy

--- app.js
+++ app.js
@@ -27,6 +27,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+passportConfig(app);
+
 app.use('/', index);
 app.use('/auth', auth);
 
